876f619b7dccd086d79c235f0a3c4664303d1ae4
Jordan Snow <snowstorm1989@gmail.com>
2017-12-06 16:06:09 -0600

    Finished

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mc = require(__dirname + '/controllers/messages_controller');
+const app = express();
+const port = 3000;
+app.use( bodyParser.json() )
+app.use( express.static( __dirname + '/../public/build' ) );
+const messagesBaseUrl = "/api/messages";
+app.post( messagesBaseUrl, mc.create );
+app.get( messagesBaseUrl, mc.read );
+app.put( `${messagesBaseUrl}/:id`, mc.update );
+app.delete( `${messagesBaseUrl}/:id`, mc.delete );
+
+
+
+
+
+
+app.listen( port, () => { console.log(`Server listening on port: ${port}`); } );
